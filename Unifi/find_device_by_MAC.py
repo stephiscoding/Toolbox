ff1d66acd145814e06b5fa3027d437283eb3b7ed
stephiscoding <30945184+stephiscoding@users.noreply.github.com>
2026-07-09 14:26:05 +0800

    fixed bugs in find_device_by_MAC.py

--- Unifi/find_device_by_MAC.py
+++ Unifi/find_device_by_MAC.py
@@ -1,6 +1,7 @@
-from lib.unifi_network_api import UnifiNetworkAPI
-import sys
 import pprint
+import sys
+
+from lib.unifi_network_api import UnifiNetworkAPI
 
 controller_url = ""
 username = ""
@@ -9,7 +10,9 @@ password = ""
 try:
     mac_to_find = sys.argv[1]
 except:
-    print("Missing argument: please provide the MAC address of the device you're looking for in the format xx:xx:xx:xx:xx:xx.")
+    print(
+        "Missing argument: please provide the MAC address of the device you're looking for in the format xx:xx:xx:xx:xx:xx."
+    )
 
 try:
     verbosity = sys.argv[2]
@@ -22,10 +25,10 @@ for site in sites:
     # get basic info for all devices at the site
     devices = api.get_devices(site)
     for device in devices:
-        if device["mac"] == mac_to_find:
-            print(f"Found {device["name"]} at site {sites[site]}!")
+        if device["mac"].lower() == mac_to_find.lower():
+            device_info = api.get_device_info(site, device["mac"])
+            print(f"Found {device_info[0]['name']} at site {sites[site]}!")
             if verbosity == "vv":
-                device_info = api.get_device_info(site, device["mac"])
                 print("Detailed device info:")
                 pprint.pprint(device_info, compact=True)
             exit(0)
