b2d2f29b0344c9ea399598bfebecb106c52100fd
stephiscoding <30945184+stephiscoding@users.noreply.github.com>
2026-07-13 12:48:00 +0800

    fixed character issues

--- Windows Admin/Update Secure Boot Cert Detect+Remediate/Remediate-SecureBoot2023.ps1
+++ Windows Admin/Update Secure Boot Cert Detect+Remediate/Remediate-SecureBoot2023.ps1
@@ -7,12 +7,12 @@ Forces a Windows machine to install the UEFI 2023 CA update from Microsoft.
 
 Special thanks to this article: https://woshub.com/updating-uefi-secure-boot-certificates-windows-faq/ for being an excellent resource here. This script is based off the info in that guide.
 #>
-
+Start-Transcript -Path "C:\ProgramData\IntuneManagementExtension\Logs\Remediate-SecureBoot2023.log" -Append
 # This registry setting tells Windows that it is allowed to install the new certificates
-Set-ItemProperty -Path ‘HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot’ -Name ‘AvailableUpdates’ -Value 0x5944
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecureBoot' -Name 'AvailableUpdates' -Value 0x5944
 
 # Run the Secure-Boot-Update scheduled task.
-Start-ScheduledTask -TaskName ‘\Microsoft\Windows\PI\Secure-Boot-Update’
+Start-ScheduledTask -TaskName '\Microsoft\Windows\PI\Secure-Boot-Update'
 
 <#
 When the user next restarts, the first part of the secure boot certificate install is run.
